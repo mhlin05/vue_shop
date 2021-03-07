78898f8bd3cdc6bdcc9d50ac98ebea828e4d117a
努力变更好 <172755874@qq.com>
2021-03-07 15:44:27 +0800

    点击登陆完成验证

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,19 +1,82 @@
 <template>
-  <div class="login_container" >
-      <div class="login_box"></div>
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 头像 -->
+      <div class="avatar_box">
+        <img src="@/assets/logo.png" alt="" />
+      </div>
+      <!-- 表单 -->
+      <el-form
+        :model="loginForm"
+        :rules="LoginRules"
+        ref="loginForm"
+        class="login_form"
+      >
+        <!-- 用户名 -->
+        <el-form-item prop="username">
+          <el-input
+            prefix-icon="el-icon-user-solid"
+            v-model="loginForm.username"
+          ></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="pwd">
+          <el-input
+            type="password"
+            prefix-icon="el-icon-lock"
+            v-model="loginForm.pwd"
+          ></el-input>
+        </el-form-item>
+        <!-- 按钮区域 -->
+        <el-form-item class="btns">
+          <el-button type="primary" @click="myLogin('loginForm')">登陆</el-button>
+          <el-button type="info" @click="resetForm('loginForm')"
+            >重置</el-button
+          >
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      // 登陆信息
+      loginForm: {
+        username: '',
+        pwd: ''
+      },
+      // 登陆规则
+      LoginRules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' }
+        ],
+        pwd: [
+          { required: true, message: '请输入密码', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    resetForm (formName) {
+      console.log('ddd')
+      this.$refs[formName].resetFields()
+    },
+    myLogin (formName) {
+      this.$refs[formName].validate(res => {
+        console.log(res)
+      })
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
 .login_container {
-    background-color: #2b4b6b;
-    height: 100%;
+  background-color: #2b4b6b;
+  height: 100%;
 }
 
 .login_box {
@@ -25,5 +88,37 @@ export default {
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
+
+  .avatar_box {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    position: absolute;
+    left: 50%;
+    box-shadow: 0 0 10px #ddd;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+
+  .login_form {
+    position: absolute;
+    bottom: 0%;
+    width: 100%;
+    padding: 0 30px;
+    box-sizing: border-box;
+  }
+}
+
+.btns {
+  display: flex;
+  justify-content: flex-end;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ import router from './router'
 import './plugins/element.js'
 // 导入全局样式表
 import './assets/css/global.css'
-
+import Axios from 'axios'
 Vue.config.productionTip = false
+// axios配置
+Vue.prototype.$http = Axios
+Axios.defaults.baseURL = ''
 
 new Vue({
   router,
