01c5614b7df993128ab62a2af88872b36c8dcb70
林铭豪 <172755874@qq.com>
2021-03-29 15:34:07 +0800

    数据报表完成

---
+++ src/components/report/Report.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="main" class="maindd"></div>
+</template>
+
+<script>
+import * as echarts from 'echarts'
+
+export default {
+  data() {
+    return {
+      options: {
+        title: {
+          text: '用户来源'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#E9EEF3'
+            }
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            boundaryGap: false
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ]
+      }
+    }
+  },
+  // 这里发现 如果created里面有请求数据函数
+  // mounted会比created先执行
+  async mounted() {
+    // 获取数据
+    const { data: res } = await this.$http.get('reports/type/1')
+    // 准备配置项
+    this.options = Object.assign(this.options, res.data)
+    console.log(this.options)
+    // console.log(this.options)
+    // 初始化echart实例
+    const myChart = echarts.init(document.getElementById('main'))
+    myChart.setOption(this.options)
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.maindd {
+  width: 100%;
+  height: 100%;
+}
+</style>
