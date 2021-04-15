de87286a2ff85a4149835a3232bbbde7a4ad366a
林铭豪 <172755874@qq.com>
2021-04-15 20:42:52 +0800

    取消发布、开发模式区别

--- vue.config.js
+++ vue.config.js
@@ -2,18 +2,17 @@ module.exports = {
   // 通过 chainWebpack 自定义打包入口
   chainWebpack: config => {
     //  发布模式
-    config.when(process.env.NODE_ENV === 'production', config => {
-      config
-        .entry('app')
-        .clear()
-        .add('./src/main-prod.js')
-    })
-
-    config.when(process.env.NODE_ENV === 'development', config => {
-      config
-        .entry('app')
-        .clear()
-        .add('./src/main-dev.js')
-    })
+    // config.when(process.env.NODE_ENV === 'production', config => {
+    //   config
+    //     .entry('app')
+    //     .clear()
+    //     .add('./src/main-prod.js')
+    // })
+    // config.when(process.env.NODE_ENV === 'development', config => {
+    //   config
+    //     .entry('app')
+    //     .clear()
+    //     .add('./src/main-dev.js')
+    // })
   }
 }
